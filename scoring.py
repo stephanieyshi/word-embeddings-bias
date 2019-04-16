f92acbb954fdcd4b08127563671b05ff3f010ce1
Stephanie Shi <stephanie.y.shi@gmail.com>
2019-04-15 23:49:07 -0400

    add scoring and simple baseline indirect bias stuff

--- scoring.py
+++ scoring.py
@@ -1,6 +1,7 @@
 import json
 import numpy as np
 from sklearn.decomposition import PCA
+from sklearn import preprocessing
 
 def compute_cosine_similarity(v1, v2):
   dot = np.dot(v1, v2)
@@ -21,18 +22,22 @@ def get_direct_bias(embedding_dict, g, words, c=1):
 
 
 def get_indirect_bias(embedding_dict, g, pairs):
-	bias = dict()
+    bias = dict()
 
-	for pair in pairs:
-		w = embedding_dict[pair[0]]
-		v = embedding_dict[pair[1]]
-		w_g = np.dot(w, g) * w
-		v_g = np.dot(v, g) * v
+    for pair in pairs:
+        w = embedding_dict[pair[0]] / np.linalg.norm(embedding_dict[pair[0]])
+        v = embedding_dict[pair[1]] / np.linalg.norm(embedding_dict[pair[1]])
+        print(np.dot(w, v))
+        # print(np.dot(w, ))
+        # # w_normalized = preprocessing.normalize(w, norm='l2')
+        # # w_normalized = preprocessing.normalize(v, norm='l2')
+        # print(np.linalg.norm(w_normalized))
+        # print(np.linalg.norm(v_normalized))
+        w_g = np.dot(w, g) * g
+        v_g = np.dot(v, g) * g
 
-		l1 = np.sqrt(np.dot(w - w_g, w - w_g))
-		l2 = np.sqrt(np.dot(v - v_g, v - v_g))
+        print(compute_cosine_similarity(w - w_g, v - v_g))
+        beta = (1 / np.dot(w, v)) * (np.dot(w, v) - compute_cosine_similarity(w - w_g, v - v_g))
+        bias[(pair[0], pair[1])] = beta
 
-		beta = (1 / np.dot(w, v)) * (np.dot(w, v) - (np.dot(w - w_g, v - v_g) / (l1 * l2)))
-		bias[(pair[0], pair[1])] = beta
-
-	return bias
+    return bias
