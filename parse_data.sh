0fa8ce11af55c1a6822a0b240757e04622e3c1fc
Stephanie Shi <stephanie.y.shi@gmail.com>
2019-04-28 15:35:07 -0400

    updated bash script

--- parse_data.sh
+++ parse_data.sh
@@ -1,23 +1,8 @@
 while IFS="," read -r a b c d e f g h i j;
 do
-    tr -d '[:punct:]' <<< "$j" | tr '[:upper:]' '[:lower:]' | awk 'ORS=FS' >> articles_lower.txt
-    echo -e >> articles_lower.txt
+    if [[ $d == 'Breitbart' ]]
+    then
+      tr -d '[:punct:]' <<< "$j" | tr '[:upper:]' '[:lower:]' | awk 'ORS=FS' >> articles_breitbart_lower.txt
+      echo >> articles_breitbart_lower.txt
+    fi
 done < articles1.csv
-
-echo 'article 1 done'
-
-while IFS="," read -r a b c d e f g h i j;
-do
-    tr -d '[:punct:]' <<< "$j" | tr '[:upper:]' '[:lower:]' | awk 'ORS=FS' >> articles_lower.txt
-    echo -e >> articles_lower.txt
-done < articles2.csv
-
-echo 'article 2 done'
-
-while IFS="," read -r a b c d e f g h i j;
-do
-    tr -d '[:punct:]' <<< "$j" | tr '[:upper:]' '[:lower:]' | awk 'ORS=FS' >> articles_lower.txt
-    echo -e >> articles_lower.txt
-done < articles3.csv
-
-echo 'article 3 done'
